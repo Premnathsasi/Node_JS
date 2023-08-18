ffd4a2dfd94f8ea28bb8534749c47d0d63640b12
Premnathsasi <premnathsasi4@gmail.com>
2023-08-18 14:59:36 +0530

    Design the User and Product relationship

--- Dynamic_Route/app.js
+++ Dynamic_Route/app.js
@@ -6,6 +6,9 @@ const bodyParser = require("body-parser");
 const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
 
+const Product = require("./models/product");
+const Users = require("./models/appUser");
+
 const cors = require("cors");
 
 const app = express();
@@ -20,10 +23,19 @@ const shopRoutes = require("./routes/shop");
 const userRoutes = require("./routes/user");
 const expenseRoutes = require("./routes/expense");
 
-const User = require("./models/User");
-
 app.use(bodyParser.json({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  Users.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -32,9 +44,22 @@ app.use("/expense", expenseRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(Users, { constraints: true, onDelete: "CASCADE" });
+Users.hasMany(Product);
+
 sequelize
+  // .sync({ force: true })
   .sync()
   .then((res) => {
+    return Users.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return Users.create({ name: "Premnath", email: "prem@test.com" });
+    }
+    return user;
+  })
+  .then((user) => {
     app.listen(3000);
   })
   .catch((err) => console.log(err));
